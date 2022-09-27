2a1ee1f4aa6c76f02021bcbc9eadea4ed60e0216
violanerd <madalyne.cross@gmail.com>
2022-09-27 11:44:50 -0500

    added webpack

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
-app.use(express.static('../client/'))
-require('./routes/htmlRoutes')(app);
+app.use(express.static("../client/dist/"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(PORT, function(){
-    console.log(`Now listening on port: ${PORT}`)
-})
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
