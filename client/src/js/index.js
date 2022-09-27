eb6729ecc8c11036725f49151eddd21c640ebb7c
violanerd <madalyne.cross@gmail.com>
2022-09-27 12:53:47 -0500

    added idb and indexeddb connection

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo2.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+import { initdb } from "./databse";
+import "../css/index.css"
 
-window.addEventListener('load', function (){
+window.addEventListener('load', function(){
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 })
 
-import "../css/index.css"
+
