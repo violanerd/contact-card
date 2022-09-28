6ea73c4872606e5f7914667d1fa6865704f93600
violanerd <madalyne.cross@gmail.com>
2022-09-28 14:29:54 -0500

    added edit and delete methods

--- client/src/js/databse.js
+++ client/src/js/databse.js
@@ -43,3 +43,34 @@ export const postDb = async(name, email, phone, profile) => {
     const result = await request;
     console.log('🚀 - data saved to the database', result);
 }
+
+export const deleteDb = async(id) => {
+    console.log('Delete from the database', id);
+
+    // Create a connection to the IndexedDB database and the version we want to use.
+    const contactDb = await openDB('contact_db', 1);
+    // Create a new transaction and specify the store and data privileges.
+    const tx = contactDb.transaction('contacts', 'readwrite');
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts')
+    // Use the .delete() method to get all data in the database.
+    const request = store.delete(id)
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result)
+    return result.value 
+}   
+export const editDb = async(id, name, email, phone, profile) => {
+    console.log('Edit the DB')
+    // Create a connection to the IndexedDB database and the version we want to use.
+    const contactDb = await openDB('contact_db', 1);
+    // Create a new transaction and specify the store and data privileges.
+    const tx = contactDb.transaction('contacts', 'readwrite');
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts')
+    // Use the .delete() method to get all data in the database.
+    const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile })
+    const result = await request;
+    console.log('result.value', result)
+    
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo2.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-import { getDb, initdb, postDb } from "./databse";
+import { getDb, initdb, postDb, deleteDb, editDb } from "./databse";
 import "../css/index.css"
 import { fetchCards } from "./card"
 
@@ -40,7 +40,14 @@ window.addEventListener('load', function(){
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-  
+    
+  //let name = document.getElementById("name").value;
+  //let phone = document.getElementById("phone").value;
+  //let email = document.getElementById("email").value;
+  //let profile = document.querySelector('input[type="radio"]:checked').value;
+
+   // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+   editDb(profileId, name, email, phone, profile);
     fetchCards();
       // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -53,3 +60,32 @@ window.addEventListener('load', function(){
   // Reload the DOM
   fetchCards();
   });
+
+window.deleteCard = (e) => {
+    //delete the card
+    console.log(e)
+    let id = parseInt(e.id)
+    deleteDb(id)
+    fetchCards()
+}
+
+window.editCard = (e) => {
+  //edit the card
+  profileId = parseInt(e.dataset.id)
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+  
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+  
+  form.style.display = "block";
+  
+  // Toggles the submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
+  
+
+}
